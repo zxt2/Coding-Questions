e831eeb3e5c0aa61ac012a8f1b1d42e3b65b37f6
zxt2 <48741275+zxt2@users.noreply.github.com>
2021-04-01 11:24:36 +0800

    Delete 1.py

--- 1.py
+++
@@ -1,7 +0,0 @@
-def max_sub_array_of_size_k(nums, k):
-    windowsum = sum(nums[:k])
-    maxsum = windowsum
-    for i in range(len(nums)-k):
-        windowsum = windowsum-nums[i]+nums[i+k]
-        maxsum = max(maxsum,windowsum)
-    return maxsum
